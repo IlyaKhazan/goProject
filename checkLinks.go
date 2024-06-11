ab9cc86038256ae5fc61776b26b16f80b07c953e
ilya.khazan <ilya.khazan@red-soft.ru>
2024-06-11 11:52:31 +0300

    add checkLinks

---
+++ checkLinks.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a string argument.")
+		return
+	}
+	fmt.Println(CheckLinks(os.Args[1]))
+}
+
+func CheckLinks(str string) string {
+	url := []byte(str)
+	values := []byte("https://")
+	asterisk := []byte("*")[0]
+	space := []byte(" ")[0]
+	pointer := 0
+	for i := range url {
+		if pointer == len(values)-1 {
+			if url[i] == space {
+				pointer = 0
+				continue
+			}
+			url[i] = asterisk
+			continue
+		}
+		if url[i] == values[pointer] {
+			pointer++
+		} else {
+			pointer = 0
+		}
+	}
+
+	return string(url)
+}
